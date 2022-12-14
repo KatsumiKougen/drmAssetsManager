e76aa961b6669b1da9834e1589be19ca18234b72
KatsumiKougen <lesanonyme1001@gmail.com>
2022-12-14 17:19:33 +0700

    No comment

---
+++ mass-renamer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+
+def Assets():
+    assets = os.listdir()
+    for i in assets:
+        if "doremonche" not in i:
+            assets.remove(i)
+    return assets
+
+currentid = int(open("./currentid", "r").read())
+target = Assets()
+template = "doraemon-%.jpg"
+
+print(
+    f"Current directory: {os.getcwd()}\n"
+    f"Current ID is {currentid}"
+)
+
+if len(target) > 0:
+    print(f"The following {len(target)} files will be renamed:")
+    for i in target:
+        print(i, end=" ")
+    confirm = input(f"\nusing the template \"{template}\"\nDo you wish to continue? (Y to confirm) ")
+    if confirm.lower() == "y":
+        for i in target:
+            os.rename(f"{i}", template.replace("%", f"{currentid:010}"))
+            currentid += 1
+        with open("./currentid", "w") as f:
+            f.write(str(currentid))
+        print(f"All {len(target)} files have been renamed.")
+    else:
+        print("Operation cancelled.")
+else:
+    print("Error: No target file.")
