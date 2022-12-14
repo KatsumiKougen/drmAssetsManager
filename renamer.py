4249abf97089b1c53a4db40f4a61f29d73dc2957
KatsumiKougen <lesanonyme1001@gmail.com>
2022-12-14 21:42:04 +0700

    No comment

--- renamer.py
+++ renamer.py
@@ -5,13 +5,13 @@ import os
 def Assets():
     assets = os.listdir()
     for i in assets:
-        if "ma_" not in i:
+        if "2nv1_" not in i:
             assets.remove(i)
     return assets
 
 currentid = int(open("./currentid", "r").read())
 target = open("./target", "r").read()[:-1].split()
-template = "misc-%.jpg"
+template = "nobita-others-%.jpg"
 
 print(
     f"Current directory: {os.getcwd()}\n"
@@ -21,11 +21,11 @@ print(
 if len(target) > 0:
     print("The following files will be renamed:")
     for i in target:
-        print(f"ma_{i}.jpg", end=" ")
+        print(f"2nv1_{i}.jpg", end=" ")
     confirm = input(f"\nusing the template \"{template}\"\nDo you wish to continue? (Y to confirm) ")
     if confirm.lower() == "y":
         for i in target:
-            os.rename(f"ma_{i}.jpg", template.replace("%", f"{currentid:010}"))
+            os.rename(f"2nv1_{i}.jpg", template.replace("%", f"{currentid:010}"))
             currentid += 1
         with open("./currentid", "w") as f:
             f.write(str(currentid))
